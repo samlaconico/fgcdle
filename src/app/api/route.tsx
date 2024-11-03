7c11a935ed6c6f314ab9bf27bb4bba95e421b0fc
samlaconico <99830026+samlaconico@users.noreply.github.com>
2024-11-02 20:30:33 -0700

    more slight fixes to eslint

--- src/app/api/route.tsx
+++ src/app/api/route.tsx
@@ -7,31 +7,29 @@ interface Special {
   input: string;
 }
 
-function shuffle( array: Special[], seed: number ) {
+function shuffle(array: Special[], seed: number) {
   let m = array.length,
     t,
     i;
 
   while (m) {
-    i = Math.floor(random(seed) * m--); // <-- MODIFIED LINE
+    i = Math.floor(random(seed) * m--);
 
-    // And swap it with the current element.
     t = array[m];
     array[m] = array[i];
     array[i] = t;
-    ++seed; // <-- ADDED LINE
+    ++seed;
   }
 
   return array;
 }
 
-function random(seed: number ) {
-  var x = Math.sin(seed++) * 10000;
+function random(seed: number) {
+  let x = Math.sin(seed++) * 10000;
   return x - Math.floor(x);
 }
 
-export async function GET(request: NextRequest) {
-
-    const list = shuffle(SpecialsList, 1);
+export async function GET() {
+  const list = shuffle(SpecialsList, 1);
   return NextResponse.json(list);
 }
